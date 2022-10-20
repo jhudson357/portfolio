c7cdfa3938863ec859df678173676fd265bf1852
James Hudson <jmh357@comcast.net>
2022-10-19 19:41:31 -0500

    create NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,21 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import Resume from './pages/Resume'
 import Projects from './pages/Projects'
+import NavBar from './components/NavBar'
 
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/contact' element={<Contact />} />
-      <Route path='/resume' element={<Resume />} />
-      <Route path='/projects' element={<Projects />} />
-    </Routes>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/resume' element={<Resume />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </>
   )
 }
 
