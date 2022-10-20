ca853aa852b790c35124cfa91ec98dcb2ed2054a
James Hudson <jmh357@comcast.net>
2022-10-19 20:22:29 -0500

    add copy and links to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,13 @@
 const Contact = () => {
   return (
-    <h1>Contact Me</h1>
+    <> 
+      <h1>Let's Connect!</h1>
+      <h2>Send me a message</h2>
+      <a href="mailto: james.hudson0357@gmail.com">james.hudson0357@gmail.com</a>
+      <h3>Socials</h3>
+      <a href="https://www.linkedin.com/in/jameshudson357/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      <a href="https://github.com/jhudson357" target="_blank" rel="noopener noreferrer">GitHub</a>
+    </>
   )
 }
 
