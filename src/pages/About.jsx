a2f6e1198a719e21f28e490167cda28a752445be
James Hudson <jmh357@comcast.net>
2022-10-19 20:03:06 -0500

    add basic copy to about page and removed copy from home page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,11 @@
 const About = () => {
   return (
-    <h1>About</h1>
+    <>
+      <h1>About</h1>
+      <p>
+        I'm a Software Engineer with a passion for finding elegant and unique digital-solutions for everyday problems. My background in email marketing and graphic design helps me think strategically through creativity and detail, while my disciplined work ethic, adaptability, and passion for excellence push me to achieve my goals. I am eager to work both individually and in teams to learn and create exciting new products through code.
+      </p>
+    </>
   )
 }
 
