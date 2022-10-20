440fa36310556a5b6b7e3382d0f4c57c7337af85
James Hudson <jmh357@comcast.net>
2022-10-19 19:51:56 -0500

    add basic info to Home page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import logo from './logo.svg'
-import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,12 @@
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <>
+      <h1>James Hudson</h1>
+      <h2>Software Engineer</h2>
+      <p>
+        I'm a Software Engineer with a passion for finding elegant and unique digital-solutions for everyday problems. My background in email marketing and graphic design helps me think strategically through creativity and detail, while my disciplined work ethic, adaptability, and passion for excellence push me to achieve my goals. I am eager to work both individually and in teams to learn and create exciting new products through code.
+      </p>
+    </>
   )
 }
 
