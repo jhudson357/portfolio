d8770cd88732a46e7575b83edd411928c360d90e
James Hudson <jmh357@comcast.net>
2022-11-10 09:44:53 -0600

    create projects data file and import into projects component

---
+++ src/data/project.js
@@ -0,0 +1,34 @@
+let projects = [
+  {
+    title: 'Wordle',
+    description: 'A recreation of the hit game, Wordle. Created using JavaScript, HTML, and CSS.',
+    image: '',
+    repositoryLink: 'https://github.com/jhudson357/wordle-unit-1-project',
+    deploymentLink: 'https://james-hudson-wordle.netlify.app/'
+  },
+  {
+    title: 'Destinations',
+    description: 'A MEN-stack vacations tracker app that allows users to keep track of destinations they have visited and would like to visit, as well as write reviews for the destinations.',
+    image: '',
+    repositoryLink: 'https://github.com/jhudson357/destinations',
+    deploymentLink: 'https://destinations.fly.dev/'
+  },
+  {
+    title: 'Fit',
+    description: 'A MERN-stack application where users are able to track their favorite exercises and search recipes pulled from the Edamam API.',
+    image: '',
+    repositoryLink: 'https://github.com/chrsteffen1/Fit-app-front-end',
+    deploymentLink: 'https://fit-planner.netlify.app/'
+  },
+  // {
+    // title: 'F1 Collector',
+  //   description: 'A Python and Django collector app where users can collect their favorite F1 teams, drivers, and team positions.',
+  //   image: '',
+  //   repositoryLink: 'https://github.com/jhudson357/f1-collector-lab',
+  //   deploymentLink: ''
+  // }
+]
+
+export {
+  projects
+}
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,17 @@
+import {projects} from '../data/project.js'
+
 const Projects = () => {
   return (
-    <h1>Projects</h1>
+    <>
+      <h1>Projects</h1>
+      <ul>
+        {projects.map(project => 
+          <li key={project.title}>
+            {project.title}
+          </li>
+        )}
+      </ul>
+    </>
   )
 }
 
