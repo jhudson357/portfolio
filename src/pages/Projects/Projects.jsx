047d433f47e7ce690a8b4661952f0d77ff194c9e
James Hudson <jmh357@comcast.net>
2022-11-14 16:43:25 -0600

    style projects list page

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -5,7 +5,7 @@ import styles from './Projects.module.css'
 const Projects = () => {
   return (
     <section className={styles.projectsContainer}>
-      <h1>Projects</h1>
+      <h1>Projects.</h1>
       <div className={styles.projects}>
         {projects.map(project => 
           <ProjectList
