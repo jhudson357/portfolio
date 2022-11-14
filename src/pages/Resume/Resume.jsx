6089d7da28642bda515d548d3dcca7fedba13cee
James Hudson <jmh357@comcast.net>
2022-11-14 17:06:14 -0600

    mobile style resume page

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -6,7 +6,7 @@ const Resume = () => {
       <div className={styles.link}>
         <a href="/James Hudson ATS Resume - Portfolio.pdf" download>Download Resume</a>
       </div>
-      <embed src="james-hudson-resume.pdf" width="800px" height="1100px" />
+      <embed src="james-hudson-resume.pdf" width="768px" height="1100px" />
     </div>
   )
 }
